24347cb926fbb0855a5d33fd5e367bcb19186d46
tlodge <tlodge@gmail.com>
2014-10-01 13:25:09 +0100

    fixed speedy drag glitch

--- js/my/d3list.js
+++ js/my/d3list.js
@@ -8,6 +8,10 @@ define(['jquery','d3'], function($,d3){
 		
 		pointerwidth = 35,
 		
+		transitionduration = 1000,
+		
+		draggedcontainer,
+		
 		colours		 = ["#880e4f","#c2185b", "#e91e63", "#f06292", "#f8bbd0"],
 		
 		mydata = [{position: 1, value: "greggs"},
@@ -53,18 +57,15 @@ define(['jquery','d3'], function($,d3){
 	  	},
 	  	
 	  	dragstart = function(d){
+	  		
 	  		startpos = (d.position - 1);
 	  		currentpos = startpos;
-	  		var container = d3.select("g." +  mydata[startpos].value);
+	  		draggedcontainer = d3.select("g." +  mydata[startpos].value);
 	   		
-	  		container.select("rect")
+	  		draggedcontainer.select("rect")
 	  			.style("fill", highlighted(startpos))
 	  			.style("stroke", highlighted(startpos));
 	  		
-	  		/*container.selectAll("circle.ranking")
-	  			.style("fill", 	highlighted(startpos))
-	  			.style("stroke", highlighted(startpos));
-	  		*/
 	  		
 	  		//make any previously selected pointers transparent
 	  		var listitems = d3.selectAll("g.listitem");
@@ -79,7 +80,7 @@ define(['jquery','d3'], function($,d3){
 	  			.style("stroke-opacity", 0.0)
 	  				
 	  		//now make the current pointer opaque	
-	  		var pointer = container.select("g")
+	  		var pointer = draggedcontainer.select("g")
 	  		
 	  		pointer.selectAll("circle")
 	  			.style("stroke-opacity", 1.0)	
@@ -90,88 +91,110 @@ define(['jquery','d3'], function($,d3){
 	  			
 	  	},
 	  	
+	  	moveneighbour = function(pos){
+	  		
+	  		var neighbourcontainer = d3.select("g." +  mydata[pos].value);
+			var newpos = -1;
+			
+			 
+			if (pos > startpos && pos > 0 && pos < mydata.length){	
+				newpos = mydata[pos].position - 1;
+				mydata[pos].position = newpos;
+				mydata[startpos].position += 1;				
+			}
+			else if (pos < startpos && pos >= 0 && pos < mydata.length-1){	
+				newpos = mydata[pos].position + 1;
+				mydata[pos].position = newpos;
+				mydata[startpos].position -= 1;
+			}
+		
+			if (newpos == -1){
+				return;
+			}
+				
+			neighbourcontainer.select("circle.outer")
+					.transition()
+					.duration(transitionduration)
+					.attr("r", multiplier(newpos) * ((height/mydata.length) / 2) - 4)
+					.attr("cy", cy(newpos))
+				
+			neighbourcontainer.select("circle.inner")
+					.transition()
+					.duration(transitionduration)
+					.attr("r", multiplier(newpos) * ((height/mydata.length) / 2) - 7)
+					.attr("cy", cy(newpos))
+					.style("fill", "#fff")
+					.style("stroke",function(d){return colour(newpos-1)})
+		
+			neighbourcontainer.select("text")
+					.transition()
+					.duration(transitionduration)
+					.attr("y", function(d){return cy(newpos)})
+					.attr("font-size", function(d){return multiplier(newpos) * 35 + "px"})	
+					.text(function(d){return newpos})
+				
+			neighbourcontainer.select("rect")
+					.transition()
+					.duration(transitionduration)
+					.attr("y", y(newpos))
+					.style("fill",function(d){return colour(newpos-1)})	
+					.style("stroke",function(d){return colour(newpos-1)})	
+		
+			neighbourcontainer.select("g")
+					.selectAll("circle")
+					.transition()
+					.duration(transitionduration)
+					.attr("cy", cy(newpos))
+		
+			neighbourcontainer.select("g")
+					.select("line")
+					.transition()
+					.duration(transitionduration)
+					.attr("y1", cy(newpos))
+					.attr("y2", cy(newpos))	
+
+	  	}
+	  	
 	  	dragit = function(d){
+	  		
+	  		
 	  		currentpos = parseInt(calcpos(d3.event.y + (height/mydata.length)/2));
-	  		var container = d3.select("g." +  mydata[currentpos].value);
-	  		var rectomove = container.select("rect");
-	  		var circletomove = container.selectAll("circle.ranking");
-	  		var pointer = container.select("g");
 	  		
-	  		
-	  		if (currentpos != startpos){
-	  			if (currentpos > startpos && currentpos > 0 && currentpos < mydata.length){	
-	  				newpos = mydata[currentpos].position - 1;
-	  				mydata[currentpos].position = newpos;
-	  				mydata[startpos].position += 1;				
+	  		//shift the rects above or below down/up
+		
+	  		if(Math.abs(currentpos-startpos) >= 1){ //if there has been some kind of movement
+	  			if (currentpos > startpos){
+	  				for (var i = startpos+1; i <= currentpos; i++){
+	  					moveneighbour(i); //this is fine!
+	  				}
+	  				
+	  			}else{
+	  				for (var i = startpos-1; i >= currentpos; i--){
+	  					moveneighbour(i);
+	  				}
 	  			}
-	  			else if (currentpos < startpos && currentpos >= 0 && currentpos < mydata.length-1){	
-	  				newpos = mydata[currentpos].position + 1;
-	  				mydata[currentpos].position = newpos;
-	  				mydata[startpos].position -= 1;
-	  			}
-	  			
-	  			container.select("circle.outer")
-	  					.transition()
-						.duration(200)
-	  					.attr("r", multiplier(newpos) * ((height/mydata.length) / 2) - 4)
-	  					.attr("cy", cy(newpos))
-	  					
-	  			container.select("circle.inner")
-	  					.transition()
-						.duration(200)
-	  					.attr("r", multiplier(newpos) * ((height/mydata.length) / 2) - 7)
-	  					.attr("cy", cy(newpos))
-	  					.style("fill", "#fff")
-	  					.style("stroke",function(d){return colour(newpos-1)})
-	  			
-	  			container.select("text")
-	  					.transition()
-						.duration(200)
-	  					.attr("y", function(d){return cy(newpos)})
-	  					.attr("font-size", function(d){return multiplier(newpos) * 35 + "px"})	
-	  					.text(function(d){return newpos})
-	  					
-	  			rectomove
-	  					.transition()
-						.duration(200)
-	  					.attr("y", y(newpos))
-	  						
-	  			pointer
-	  					.selectAll("circle")
-	  					.transition()
-	  					.duration(200)
-	  					.attr("cy", cy(newpos))
-	  			
-	  			pointer
-	  					.select("line")
-	  					.transition()
-	  					.duration(200)
-	  					.attr("y1", cy(newpos))
-	  					.attr("y2", cy(newpos))	
-	  					
-	  		    mydata.sort(function(a,b){return a.position > b.position})	
+	  		 	mydata.sort(function(a,b){return a.position > b.position})
 	  			startpos = currentpos;
 	  		}	
 	  		
 	  		vcenter   = (height/mydata.length)/2 - (rectmargin/2);
 	  		maxheight = height - vcenter;
 	  		
-	  		rectomove
+	  		draggedcontainer.select("rect")
 	   			.attr("x", d.x = 0)
 	   			.attr("y", d.y = Math.min(maxheight, Math.max(0,d3.event.y)))
 	   		
 
-			circletomove
+			draggedcontainer.selectAll("circle.ranking")
 				.attr("cx", d.x = 0)
 				.attr("cy", d.y = Math.min(maxheight + vcenter, Math.max(vcenter, d3.event.y + vcenter)))
 	  		
 	  		
-	  		
-	  		pointer.selectAll("circle")
+	  		draggedcontainer.select("g").selectAll("circle")
 	  			.attr("cy", function(d){return Math.min(maxheight+vcenter,
 	  													Math.max(vcenter, d3.event.y + vcenter))});
 	  		
-	  		pointer.selectAll("line")
+	  		draggedcontainer.select("g").selectAll("line")
 	  			
 	  			.attr("y1", function(d){return Math.min(maxheight + vcenter,
 	  										    		Math.max(vcenter, d3.event.y + vcenter))})
@@ -179,47 +202,46 @@ define(['jquery','d3'], function($,d3){
 	  			.attr("y2", function(d){return Math.min(maxheight + vcenter,
 	  										    		Math.max(vcenter, d3.event.y + vcenter))});							  	
 	  			
-			container.select("text")
+			draggedcontainer.select("text")
 				.attr("y", function(d){return Math.min(maxheight+vcenter,
 	  													Math.max(vcenter, d3.event.y + vcenter))})
+	  			.text(function(d){return mydata[currentpos].position})
 
-	  			
 	   	},
 	   			
 	   	
 	   	dragend = function(d,i){
-	   		
-	   		
-	   		var container = d3.select("g." +  mydata[currentpos].value);
-	   		var pointer   = container.select("g");
-	   		
-	   		container.select("rect")
+	   	
+	   		draggedcontainer.select("rect")
 	  				.attr("y", y(mydata[currentpos].position))	
 	  		
-	  		container.select("rect")
+	  		draggedcontainer.select("rect")
 	  				.transition()
-	  				.duration(500)
+	  				.duration(transitionduration)
 	  				.style("fill", colour(currentpos))
 	  				.style("stroke", colour(currentpos));
 	  		
-	  	    container.selectAll("circle.ranking")
+	  	    draggedcontainer.selectAll("circle.ranking")
 	  				.attr("cy", cy(mydata[currentpos].position))
 	  				
-	  		container.select("circle.outer")
+	  		draggedcontainer.select("circle.outer")
 	  				.attr("r", multiplier(mydata[currentpos].position) * ((height/mydata.length) / 2) - 4)
 	  				
 	  				
-	  		container.select("circle.inner")
+	  		draggedcontainer.select("circle.inner")
 	  				.attr("r", multiplier(mydata[currentpos].position) * ((height/mydata.length) / 2) - 7)
 	  				.style("fill", "#fff")
 	  				.style("stroke",function(d){return colour(d.position - 1)})
 	  		
-	  		container.select("text")
+	  		draggedcontainer.select("text")
 				.attr("y", function(d){return cy(mydata[currentpos].position)})
 	  			.attr("font-size", function(d){return multiplier(currentpos) * 35 + "px"})	
-	  			.text(function(d){return mydata[currentpos].position})
+	  		//	.text(function(d){return mydata[currentpos].position})
+	  		
+	  		var pointer   = draggedcontainer.select("g");
+	  		
 	  		pointer.selectAll("circle")
-	  				.attr("cy", cy(mydata[currentpos].position))
+	  				.attr("cy", cy(mydata[currentpos].position));
 	  		
 	  		pointer.select("circle.inner")
 	  				.style("fill", function(d){return colour(d.position-1)});
